4369200ae7f63458baaaf689dc1ce6803df2fce3
Village17317 <Village0427@gmail.com>
2018-10-26 11:22:32 +0900

    NamedArrayの追加

---
+++ Assets/GFF2019/Scripts/Attribute/PropertyAttribute/NamedArrayAttribute.cs
@@ -0,0 +1,29 @@
+﻿/*作成者     ：村上 和樹
+ *機能説明   ：配列のindexを別名で指定
+ *初回作成日 ：2018/10/25
+ *更新日     ：2018/10/25
+*/
+namespace Village
+{
+    public class NamedArrayAttribute : UnityEngine.PropertyAttribute
+    {
+        /// <summary>
+        /// 各Indexの名前
+        /// </summary>
+        public string[] IndexNames
+        {
+            get;
+            private set;
+        }
+        
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="names">描画したい名前</param>
+        public NamedArrayAttribute(params string[] names)
+        {
+            IndexNames = names;
+        }
+
+    }
+}
---
+++ Assets/GFF2019/Scripts/Attribute/PropertyDrawer/Editor/NamedArrayDrawer.cs
@@ -0,0 +1,42 @@
+﻿/*作成者     ：村上 和樹
+ *機能説明   ：配列のindexを別名で描画
+ *更新日     ：2018/10/25
+ *初回作成日 ：2018/10/25
+*/
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Village
+{
+    [CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+    public class NamedArrayDrawer : PropertyDrawer
+    {
+        /// <summary>
+        /// Inspectorの描画
+        /// </summary>
+        /// <param name="rect">描画範囲</param>
+        /// <param name="property">対応するもの</param>
+        /// <param name="label">通常時のラベル</param>
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            try
+            {
+                int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+                EditorGUI.PropertyField(rect, property, new GUIContent(Attribute.IndexNames[pos]));
+            }
+            catch
+            {
+                EditorGUI.PropertyField(rect, property, label);
+            }
+        }
+
+        /// <summary>
+        /// Attributeの変換した値
+        /// </summary>
+        private NamedArrayAttribute Attribute
+        {
+            get{ return attribute as NamedArrayAttribute; }
+        }
+    }
+}
