fecc42d28a8ae74705e7f8f1a6939a9c5dd9c687
Village17317 <Village0427@gmail.com>
2018-10-26 13:15:41 +0900

    SceneObjectの追加

---
+++ Assets/GFF2019/Scripts/SceneObject/Editor/SceneObjectEditor.cs
@@ -0,0 +1,70 @@
+﻿/*作成者     ：村上 和樹
+ *機能説明   ：SceneObjectのEditor上での振る舞い
+ *初回作成日 ：2018/10/26
+ *更新日     ：2018/10/26
+*/
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Village
+{
+    [CustomPropertyDrawer(typeof(SceneObject))]
+    public class SceneObjectEditor : PropertyDrawer
+    {
+        private const string VariableName = "_sceneName";    // 変数名
+        private const string LogMsg       = "can't be used"; // エラーメッセージ
+        
+        /// <summary>
+        /// SceneAssetの取得
+        /// </summary>
+        /// <param name="sceneObjectName">取得したいシーンの名前</param>
+        private SceneAsset GetSceneObject(string sceneObjectName)
+        {
+            // 値が空、もしくはNullの場合 nullを返す
+            if (string.IsNullOrEmpty(sceneObjectName)) { return null; }
+
+            // Scenes In Buildに登録されているかをチェック
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                var scene = EditorBuildSettings.scenes[i];
+                if (scene.path.IndexOf(sceneObjectName) != -1)
+                {
+                    return AssetDatabase.LoadAssetAtPath(scene.path,typeof(SceneAsset)) as SceneAsset;
+                }
+            }
+            //　登録されていない場合 nullを返す
+            Debug.Log(sceneObjectName + " " +  LogMsg);
+            return null;
+        }
+
+        /// <summary>
+        /// Inspector上での表示
+        /// </summary>
+        /// <param name="rect">表示位置</param>
+        /// <param name="property">対応するもの</param>
+        /// <param name="label">ラベル</param>
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            var prop     = property.FindPropertyRelative(VariableName);
+            var sceneObj = GetSceneObject(prop.stringValue);
+            var newScene = EditorGUI.ObjectField(rect, label, sceneObj, typeof(SceneAsset), false);
+            
+            // sceneがsetされていない時
+            if (newScene == null)
+            {
+                prop.stringValue = string.Empty;
+            }
+            else
+            {
+                // 現在setされているものと新しくセットしたものが同じとき
+                if (newScene.name == prop.stringValue)     { return; }
+                // Scenes In Buildに登録されていない場合
+                if (GetSceneObject(newScene.name) == null) { return; }
+
+                prop.stringValue = newScene.name;
+            }
+        }
+        
+    }
+}
---
+++ Assets/GFF2019/Scripts/SceneObject/SceneObject.cs
@@ -0,0 +1,34 @@
+﻿/*作成者     ：村上 和樹
+ *機能説明   ：
+ *初回作成日 ：2018/10/26
+ *更新日     ：2018/10/26
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Village
+{
+    [System.Serializable]
+    public class SceneObject
+    {
+        [SerializeField] private string _sceneName;
+
+        /// <summary>
+        /// 暗黙的にSceneObjectをstringに変更
+        /// </summary>
+        public static implicit operator string(SceneObject sceneObject)
+        {
+            return sceneObject._sceneName;
+        }
+
+        /// <summary>
+        /// 暗黙的にstringをSceneObjectに変更
+        /// </summary>
+        public static implicit operator SceneObject(string sceneName)
+        {
+            return new SceneObject(){ _sceneName = sceneName };
+        }
+
+    }
+}
